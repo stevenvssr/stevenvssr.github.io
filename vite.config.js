4722646806a5873fad4cbf9024b3d5a5288ac824
Steven Visser <steven_v1987@live.nl>
2025-11-06 11:45:30 +0100

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // Important for user sites on GitHub Pages
   plugins: [react()],
   css: {
     preprocessorOptions: {
