acb58e9e3f677dbdfeec20e99be4ee4dd0b4eb22
Steven Visser <steven_v1987@live.nl>
2025-11-06 11:55:15 +0100

    Update site

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // Important for user sites on GitHub Pages
+  base: "/", // user site
   plugins: [react()],
   css: {
     preprocessorOptions: {
